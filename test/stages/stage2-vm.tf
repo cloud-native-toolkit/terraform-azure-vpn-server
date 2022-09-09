94e54b9e488e76efc95786af63742270e9fdc62e
hemang-ibm <103431190+hemang-ibm@users.noreply.github.com>
2022-09-09 18:33:41 +0530

    Update stage2-vm.tf
    vnet_name added in stage2_vm test case.

--- test/stages/stage2-vm.tf
+++ test/stages/stage2-vm.tf
@@ -3,5 +3,6 @@ module "azure-vm" {
 
   name_prefix         = "${local.name_prefix}-vm"
   resource_group_name = module.resource_group.name
+  virtual_network_name = module.vnet.name
   subnet_id           = module.subnets.id
 }
