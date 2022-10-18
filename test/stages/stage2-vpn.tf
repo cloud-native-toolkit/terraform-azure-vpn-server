09f392e73f5cc2c0f199ece6d85bb6acc1a087a5
rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-10-18 11:45:55 +1100

    Updates test stages for ssh keys
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

---
+++ test/stages/stage2-vpn.tf
@@ -0,0 +1,10 @@
+module "azure-vm-opnvpn" {
+  source = "./module"
+
+  name_prefix          = "${local.name_prefix}-openvpn"
+  resource_group_name  = module.resource_group.name
+  virtual_network_name = module.vnet.name
+  subnet_id            = module.subnets.id
+  pub_ssh_key_file     = module.ssh-keys.pub_key_file
+  private_key_file     = module.ssh-keys.private_key_file
+}
