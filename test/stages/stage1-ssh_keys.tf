81fa670297e119f0d70f75407766671e842aff62
rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-10-18 12:53:39 +1100

    Fixes test ssh test stage
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

--- test/stages/stage1-ssh_keys.tf
+++ test/stages/stage1-ssh_keys.tf
@@ -1,7 +1,6 @@
 module "test-keys" {
   source = "github.com/cloud-native-toolkit/terraform-azure-ssh-key?ref=v1.0.6"
 
-  key_name            = "${local.name_prefix}-key"
   resource_group_name = module.resource_group.name
   region              = module.resource_group.region
   store_path          = "${path.cwd}/.ssh"
