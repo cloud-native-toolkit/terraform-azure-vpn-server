d65a3ecc97167f00b28162be1dca76b45741ae7d
rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-10-19 16:45:49 +1100

    Adds descriptions to output variables
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,29 @@
 output "id" {
-  value = module.openvpn-server.id
+  value       = module.openvpn-server.id
+  description = "The Azure id of the created VPN server"
 }
 
 output "vm_public_ip" {
-  value = module.openvpn-server.vm_public_ip
+  value       = module.openvpn-server.vm_public_ip
+  description = "The public IP address of the created VPN server"
 }
 
 output "vm_public_fqdn" {
-  value = module.openvpn-server.vm_public_fqdn
+  value       = module.openvpn-server.vm_public_fqdn
+  description = "The FQDN of the public IP address"
 }
 
 output "admin_username" {
   depends_on = [
     module.openvpn-server
   ]
-  value = var.admin_username
+  value       = var.admin_username
+  description = "The administrator username of the created VPN server"
 }
 
 output "vm_private_ip" {
-  value = module.openvpn-server.vm_private_ip
+  value       = module.openvpn-server.vm_private_ip
+  description = "The private IP address of the created VPN server"
 }
 
 output "client_config_file" {
@@ -26,4 +31,5 @@ output "client_config_file" {
     null_resource.download_client_config
   ]
   value = "${path.cwd}/${local.client_config_file}"
+  description = "The full path and filename of the created VPN client connection file"
 }
